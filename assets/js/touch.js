25f9d25ba0829a70567fb1479764cd32d4541629
AlexNeige <lagealex9@gmail.com>
2024-02-08 16:24:27 +0100

    modification

--- assets/js/touch.js
+++ assets/js/touch.js
@@ -1,15 +1,15 @@
 var touch = document.getElementById("touch");
-
+var debug = document.getElementById("debug");
 var hammertime = new Hammer(document.body, {});
 var ofLeft = touch.offsetLeft;
 var ofTop = touch.offsetTop;
 hammertime.on('pan pinch', function (event) {
     console.dir(touch.offsetLeft)
     touch.offsetLeft = ofLeft
-
     let distance = event.distance;
     let additionalEvent = event.additionalEvent;
     console.log(additionalEvent, distance);
+    debug.innerText = additionalEvent;
     //"panright" "panleft" "panup" "pandown"
     switch (additionalEvent) {
         case "panright":
